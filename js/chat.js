f954c7f94d1f1609cfeab999b4dad8d40d84ebcb
Ryan Tucker <ryantucker0@gmail.com>
2015-09-13 06:09:03 -0400

    I believe there is now full functionality

--- js/chat.js
+++ js/chat.js
@@ -2,55 +2,71 @@ var messagesRef = new Firebase("https://glaring-fire-8593.firebaseio.com");
 var newMessage = ('new-message');
 var authData = messagesRef.getAuth();
 var message = ('messages');
-
+console.log("refresh");
 var $newMessage = $('#new-message');
 var $username = authData.twitter.displayName;
 var $messages = $('#messages');
 
+var room = window.location.hash.slice(1);
+if(room){
+startChat(room)
 
-messagesRef.child('user-chat-room').limitToLast(10).on('child_added', function (snapshot) {
-   //GET DATA
-   var data = snapshot.val();
-   console.log ("8===>" + data);
-   var username = data.name;
-   var message = (" " + data.text);
-   console.log("HIII" + message);
+ }
 
-   //CREATE ELEMENTS MESSAGE & SANITIZE TEXT
-   var messageElement = $("<li>");
-   var nameElement = $("<strong class='example-chat-username'></strong>");
-   nameElement.text(username);
-   messageElement.text(message).prepend(nameElement);
+function getUrl(){
+  var newUrl = (document.getElementById('urlName').value);
+  console.log(newUrl);
+  testLogin();
+  window.open("#"+newUrl,"_self");
+  clearInit();
+  startChat(newUrl);
+}
 
-   //ADD MESSAGE TO LIST
-   $messages.append(messageElement);
-   console.log("8888" + messageElement);
+function startChat(room){
 
-   //SCROLL TO BOTTOM OF CHAT BOX
-   $messages[0].scrollTop = $messages[0].scrollHeight;
- });
+  messagesRef.child(room).limitToLast(10).on('child_added', function (snapshot) {
+     //GET DATA
+     var data = snapshot.val();
+     console.log ("8===>" + data);
+     var username = data.name;
+     var message = (" " + data.text);
+     console.log("HIII" + message);
+
+     //CREATE ELEMENTS MESSAGE & SANITIZE TEXT
+     var messageElement = $("<li>");
+     var nameElement = $("<strong class='example-chat-username'></strong>");
+     nameElement.text(username);
+     messageElement.text(message).prepend(nameElement);
+
+     //ADD MESSAGE TO LIST
+     $messages.append(messageElement);
+     console.log("8888" + messageElement);
+
+     //SCROLL TO BOTTOM OF CHAT BOX
+     $messages[0].scrollTop = $messages[0].scrollHeight;
+   });
 
 
- /*
- * Listen for user input
- *
- * This method listens for each keypress
- * on the message input field and saves
- * the data to Firebase when content is submitted.
- */
+   /*
+   * Listen for user input
+   *
+   * This method listens for each keypress
+   * on the message input field and saves
+   * the data to Firebase when content is submitted.
+   */
 
- $newMessage.keypress(function (e) {
-   // GET FIELD VALUES
-   var username = $username;
-   var message = $newMessage.val().trim();
+   $newMessage.keypress(function (e) {
+     // GET FIELD VALUES
+     var username = $username;
+     var message = $newMessage.val().trim();
 
-   console.log('===========', username, message);
+     console.log('===========', username, message);
 
-   // SAVE MESSAGE WHEN 'ENTER' IS PRESSED
-   var x = location.pathname;
-   var urlsplit = x.split("/").slice(-1)[0];
-   if (e.keyCode == 13 && message.length) {
-     messagesRef.child(urlsplit).push().set({name:username, text:message});
-     $newMessage.val('');
-   }
- });
+     // SAVE MESSAGE WHEN 'ENTER' IS PRESSED
+
+     if (e.keyCode == 13 && message.length) {
+       messagesRef.child(room).push().set({name:username, text:message});
+       $newMessage.val('');
+     }
+   });
+ }
